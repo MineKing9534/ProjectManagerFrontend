091b1ef19ec02b5c9a152c6e5290a3c496dad87d
MineKing <mineking9534@gmail.com>
2024-05-10 20:21:32 +0200

    Fix format

--- src/pages/account/AccountPage.tsx
+++ src/pages/account/AccountPage.tsx
@@ -85,7 +85,7 @@ export default function AccountPage() {
 
 					</CardBody>
 					<CardFooter className="p-2 flex-shrink-0 flex gap-2">
-					Ihre ID: <b className="select-text">{ user?.id }</b>
+						Ihre ID: <b className="select-text">{ user?.id }</b>
 					</CardFooter>
 				</Card>
 
