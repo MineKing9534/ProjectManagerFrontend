d6edf59573fc29e2f6fc3422e6c909fa8a5eb54f
MineKing <mineking9534@gmail.com>
2024-06-04 18:56:52 +0200

    Add team delete button

--- src/pages/account/resource/teams/TeamSettingsPage.tsx
+++ src/pages/account/resource/teams/TeamSettingsPage.tsx
@@ -1,11 +1,11 @@
-import { Button, Card, CardBody, CardFooter, CardHeader, CircularProgress, Divider, Input, useDisclosure } from "@nextui-org/react"
+import { Button, Card, CardBody, CardFooter, CardHeader, CircularProgress, Divider, Input, Modal, ModalBody, ModalContent, ModalFooter, ModalHeader, useDisclosure } from "@nextui-org/react"
 import ErrorModal from "../../../../components/ErrorModal.tsx"
 import { useParams } from "react-router-dom"
 import { useRest } from "../../../../hooks/useRest.ts"
 import { Team } from "../../../../types/Team.ts"
 import { useNavigate } from "react-router"
 import Spinner from "../../../../components/Spinner.tsx"
-import { PencilLine, Save } from "lucide-react"
+import { PencilLine, Save, Trash2 } from "lucide-react"
 import { useState } from "react"
 import BackButton from "../../../../components/BackButton.tsx"
 
@@ -15,6 +15,7 @@ export default function TeamSettingsPage() {
 	const params = useParams()
 	const id = params.id
 
+	const { isOpen, onOpen, onOpenChange } = useDisclosure()
 	const { isOpen: isErrorOpen, onOpen: onErrorOpen, onOpenChange: onErrorOpenChange } = useDisclosure()
 
 	const { data, error, get } = useRest<Team>(`/teams/${ id }`, {
@@ -25,9 +26,9 @@ export default function TeamSettingsPage() {
 		}
 	})
 
-	const { state: editState, error: editError, patch } = useRest(`/teams/${ id }`, {
+	const { state: editState, error: editError, patch, del } = useRest(`/teams/${ id }`, {
 		onError: onErrorOpen,
-		onSuccess: get
+		onSuccess: result => result ? get() : navigate("/@me/teams")
 	})
 
 	const [ name, setName ] = useState("")
@@ -56,15 +57,26 @@ export default function TeamSettingsPage() {
 					/>
 				</> }
 			</CardBody>
-
-			{ (name !== data?.name && name.length >= 3) && <>
-				<Divider/>
-				<CardFooter className="flex w-full justify-end py-2">
-					<Button size="sm" color="primary" isLoading={ editState === "loading" } className="font-bold" spinner={ <Spinner/> } startContent={ <Save strokeWidth="2.5px" height="20px"/> } onPress={ update }>Speichern</Button>
-				</CardFooter>
-			</>}
+			<Divider/>
+			<CardFooter className="flex w-full justify-between py-2">
+				<Button size="sm" color="danger" isLoading={ editState === "loading" } className="font-bold" spinner={ <Spinner/> } startContent={ <Trash2 strokeWidth="2.5px" height="20px"/> } onPress={ onOpen }>Löschen</Button>
+				{ (name !== data?.name && name.length >= 3) && <Button size="sm" color="primary" isLoading={ editState === "loading" } className="font-bold" spinner={ <Spinner/> } startContent={ <Save strokeWidth="2.5px" height="20px"/> } onPress={ update }>Speichern</Button> }
+			</CardFooter>
 
 			<ErrorModal error={ (error || editError)! } isOpen={ isErrorOpen } onOpenChange={ onErrorOpenChange } onClose={ () => error && navigate("/@me/teams") }/>
+
+			<Modal isOpen={ isOpen } onOpenChange={ onOpenChange }>
+				<ModalContent>
+					<ModalHeader className="py-2">{ data?.name } Löschen</ModalHeader>
+					<Divider/>
+					<ModalBody className="block">
+						Soll das Team <b>{ data?.name }</b> wirklich gelöscht werden? Diese Aktion kann nicht rückgängig gemacht werden!
+					</ModalBody>
+					<ModalFooter className="p-2">
+						<Button size="sm" color="danger" isLoading={ editState === "loading" } className="font-bold" spinner={ <Spinner/> } startContent={ <Trash2 strokeWidth="2.5px" height="20px"/> } onPress={ () => del() }>Löschen</Button>
+					</ModalFooter>
+				</ModalContent>
+			</Modal>
 		</Card>
 	)
 }
