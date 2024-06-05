c97daa92f05392c2ff7162802549c80c17adb717
MineKing <mineking9534@gmail.com>
2024-06-05 09:51:06 +0200

    Improve AccountLayout.tsx

--- src/pages/account/AccountLayout.tsx
+++ src/pages/account/AccountLayout.tsx
@@ -1,27 +1,27 @@
-import { useUser } from "../../hooks/useUser.ts"
+import { useUserRequest } from "../../hooks/useUser.ts"
 import { Outlet, useLocation, useNavigate } from "react-router"
-import { Divider, Modal, ModalBody, ModalContent, ModalHeader } from "@nextui-org/react"
+import { CircularProgress, Divider, Modal, ModalBody, ModalContent, ModalHeader } from "@nextui-org/react"
 
 export default function AccountLayout() {
 	const { pathname } = useLocation()
 	const navigate = useNavigate()
-	const user = useUser()
 
-	if(!user) return (
-		<Modal isOpen onClose={ () => navigate("/", { state: pathname }) }>
-			<ModalContent>
-				<ModalHeader className="py-2 text-danger">Fehler</ModalHeader>
-				<Divider/>
-				<ModalBody className="py-5">
-					Sie sind nicht angemeldet! Melden Sie sich zunächst an, bevor Sie diese Seite verwenden können!
-				</ModalBody>
-			</ModalContent>
-		</Modal>
-	)
+	const { state, data } = useUserRequest()!
 
+	if(state === "loading") return <CircularProgress className="m-auto"/>
 	return (
 		<>
-			<Outlet/>
+			{ data ? <Outlet/> :
+				<Modal isOpen onClose={ () => navigate("/", { state: pathname }) }>
+					<ModalContent>
+						<ModalHeader className="py-2 text-danger">Fehler</ModalHeader>
+						<Divider/>
+						<ModalBody className="py-5">
+							Sie sind nicht angemeldet! Melden Sie sich zunächst an, bevor Sie diese Seite verwenden können!
+						</ModalBody>
+					</ModalContent>
+				</Modal>
+			}
 		</>
 	)
 }
