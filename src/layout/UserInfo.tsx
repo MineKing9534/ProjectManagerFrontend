fd02ec57316559fa8a9096478eb62779dbe90d3b
MineKing <mineking9534@gmail.com>
2024-06-06 10:36:49 +0200

    Fix navigation for sub-paths

--- src/layout/UserInfo.tsx
+++ src/layout/UserInfo.tsx
@@ -29,9 +29,9 @@ export default function UserInfo({ user }: { user: User }) {
 					</DropdownSection>
 
 					<DropdownSection showDivider>
-						{ (user.admin && <DropdownItem startContent={ <Users width="20px"/> } className={ pathname === "/@me/users" ? "[&>span]:font-bold" : "" } as={ Link } to="/@me/users">Nutzerliste</DropdownItem>) as never }
-						<DropdownItem startContent={ <CalendarDays width="20px"/> } className={ pathname === "/@me/meetings" ? "[&>span]:font-bold" : "" } as={ Link } to="/@me/meetings">Veranstaltungen</DropdownItem>
-						<DropdownItem startContent={ <Book width="20px"/> } className={ pathname === "/@me/teams" ? "[&>span]:font-bold" : "" } as={ Link } to="/@me/teams">Teams</DropdownItem>
+						{ (user.admin && <DropdownItem startContent={ <Users width="20px"/> } className={ pathname.startsWith("/@me/users") ? "[&>span]:font-bold" : "" } as={ Link } to="/@me/users">Nutzerliste</DropdownItem>) as never }
+						<DropdownItem startContent={ <CalendarDays width="20px"/> } className={ pathname.startsWith("/@me/meetings") ? "[&>span]:font-bold" : "" } as={ Link } to="/@me/meetings">Veranstaltungen</DropdownItem>
+						<DropdownItem startContent={ <Book width="20px"/> } className={ pathname.startsWith("/@me/teams") ? "[&>span]:font-bold" : "" } as={ Link } to="/@me/teams">Teams</DropdownItem>
 					</DropdownSection>
 
 					<DropdownItem startContent={ <LogOut width="20px"/> } color="danger" onPress={ () => {
