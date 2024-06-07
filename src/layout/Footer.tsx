7b5788e87acb3fcd27161c92b5d2c763dcd8cb11
MineKing <mineking9534@gmail.com>
2024-06-07 21:43:43 +0200

    Add impress and privacy

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -4,9 +4,14 @@ export default function Footer() {
 	return (
 		<footer className="w-full h-[35px] select-none flex-shrink-0">
 			<Divider className="absolute"/>
-			<div className="z-40 flex px-6 gap-4 w-full flex-row relative flex-nowrap whitespace-nowrap items-center justify-between h-full max-w-full">
-				<span className="justify-start">© 2024 { import.meta.env._OWNER }</span>
-				<span className="justify-end hidden md:block"><Link showAnchorIcon className="text-foreground" target="_blank" href={ `https://github.com/${ import.meta.env._REPOSITORY }` }>{ import.meta.env._REPOSITORY }</Link></span>
+			<div className="z-40 flex px-6 gap-10 w-full flex-row relative flex-nowrap whitespace-nowrap items-center justify-between h-full max-w-full">
+				<span className="hidden md:block">© 2024 { import.meta.env._OWNER }</span>
+				<div className="flex gap-4 justify-between w-full md:w-fit">
+					<Link href={ import.meta.env._IMPRESS } className="text-foreground" target="_blank">Impressum</Link>
+					<Link href={ import.meta.env._PRIVACY } className="text-foreground" target="_blank">Datenschutz</Link>
+				</div>
+				<span className="hidden md:block flex-grow"/>
+				<span className="hidden md:block"><Link showAnchorIcon className="text-foreground" target="_blank" href={ `https://github.com/${ import.meta.env._REPOSITORY }` }>{ import.meta.env._REPOSITORY }</Link></span>
 			</div>
 		</footer>
 	)
